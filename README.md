f3ef7313d2325be5606d482e1aa47906f7e8f777
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-06-23 15:37:06 +0900

    first import

---
+++ README.md
@@ -0,0 +1,23 @@
+# wmi2struct
+
+Generate Go struct from WMI class name
+
+## Usage
+
+```
+wmi2struct -o mypackage Win32_PerfFormattedData_PerfNet_Server
+```
+
+## Installation
+
+```
+go get github.com/mattn/wmi2struct
+```
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"go/format"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type Property struct {
+	Name string `xml:"NAME,attr"`
+	Type string `xml:"TYPE,attr"`
+}
+
+type class struct {
+	Name     string     `xml:"NAME,attr"`
+	Property []Property `xml:"PROPERTY"`
+}
+
+type result struct {
+	Class class `xml:"RESULTS>CIM>CLASS"`
+}
+
+func typeName(s string) string {
+	switch s {
+	case "boolean":
+		return "bool"
+	case "char16":
+		return "uint16"
+	case "datetime":
+		return "time.Time"
+	case "object":
+		return "interface{}"
+	case "real32":
+		return "float32"
+	case "real64":
+		return "float64"
+	case "reference":
+		return "interface{}"
+	case "sint16":
+		return "int16"
+	case "sint32":
+		return "int32"
+	case "sint8":
+		return "int8"
+	case "string":
+		return "string"
+	case "uint8":
+		return "uint8"
+	case "uint16":
+		return "uint16"
+	case "uint32":
+		return "uint32"
+	case "uint64":
+		return "uint16"
+	}
+	return s
+}
+
+func main() {
+	var pkg, out string
+	flag.StringVar(&pkg, "p", "main", "package")
+	flag.StringVar(&out, "o", "", "output filename")
+	flag.Parse()
+
+	var buf bytes.Buffer
+	for _, arg := range flag.Args() {
+		cmd := exec.Command("wmic", "class", arg, "get", "/format:RAWXML")
+		b, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal(err)
+		}
+		var r result
+		err = xml.Unmarshal(b, &r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(&buf, "package %s\n", pkg)
+		fmt.Fprintf(&buf, "type %s struct {\n", r.Class.Name)
+		for _, p := range r.Class.Property {
+			fmt.Fprintf(&buf, "\t%s\t%s\n", p.Name, typeName(p.Type))
+		}
+		fmt.Fprintln(&buf, "}\n")
+	}
+
+	b, err := format.Source(buf.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var w io.Writer = os.Stdout
+	if out != "" {
+		f, err := os.Create(out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		w = f
+	}
+	w.Write(b)
+}
